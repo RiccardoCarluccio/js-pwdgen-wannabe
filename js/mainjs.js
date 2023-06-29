35d5b24356c87e7884529f02a4e1c74568d52b5f
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-06-29 15:37:21 +0200

    Bonus

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,7 +1,8 @@
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
 const favouriteColour = prompt('Inserisci il tuo colore preferito');
+const age = prompt('Inserisci la tua età');
 
-let password = firstName + lastName + favouriteColour + parseInt('21');
+let password = firstName + lastName + favouriteColour + (parseInt(age) + 21);
 
 document.getElementById('pass-generated').innerHTML = `la tua password è: ${password}`;
